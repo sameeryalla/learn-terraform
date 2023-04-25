c52c0d9d9aa0c8785af68ee56d02eb4042ddce9a
sameeryalla <sameeryalla@gmail.com>
2023-04-25 22:23:44 +0530

    variable declaration in terraform2

--- 01_variables/vars.tf
+++ 01_variables/vars.tf
@@ -9,4 +9,11 @@ output "sample_string" {
 }
 ##shell scripting equivalent is echo sample_string=$sample_string
 
-
+##in the above case we are printing just variable, if variable is combination of some strings then following is the syntax
+#${} is mandatory to include variavble in between a string
+variable "sample_string1"  {
+  default="Hello world"
+}
+output "sample_string1" {
+  value="value of sample string=${var.sample_string}"
+}
