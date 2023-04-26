022db26642169cd0e459be5a00ace85c48150b42
sameeryalla <sameeryalla@gmail.com>
2023-04-26 22:01:36 +0530

    variable declaration in terraform 8

--- 01_variables/vars.tf
+++ 01_variables/vars.tf
@@ -1,20 +1,6 @@
 variable "sample_string"  {
   default="Hello world"
 }
-##shell scripting equivalent is sample_string="Hello world"
-
-##to print in shell script
-output "sample_string1" {
-  value=var.sample_string
-}
-##shell scripting equivalent is echo sample_string=$sample_string
-
-##in the above case we are printing just variable, if variable is combination of some strings then following is the syntax
-#${} is mandatory to include variavble in between a string
-
-output "sample_string2" {
-  value="value of sample string=${var.sample_string}"
-}
 
 
 # different data type
@@ -36,12 +22,6 @@ variable "sample_list" {
   ]
 }
 
-output "sample_list2"{
-  value=var.sample_list[1]
-}
-
-
-
 #dictonary/MAP
 variable "sample_dict" {
   default={
@@ -51,10 +31,5 @@ variable "sample_dict" {
     number2=123
   }
 }
-output "sample_dict1" {
-  value = "value from dictopnary is ${var.sample_dict.number2}"
-}
 
-output "sample_dict2" {
-  value = "value from dictopnary is ${var.sample_dict.boolean}"
-}
+variable "env" {}
