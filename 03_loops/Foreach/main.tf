8dbadffb68882c326d0e62bbbe5178a07511388a
sameeryalla <sameeryalla@gmail.com>
2023-05-02 22:30:46 +0530

    For each loop execution in terraform3

--- 03_loops/Foreach/main.tf
+++ 03_loops/Foreach/main.tf
@@ -8,8 +8,9 @@ variable "fruits"{
 
 resource "null_resource" "fruits"{
 
-
+  count=length(var.fruits)
   provisioner "local-exec" {
+    command="echo fruitname= ${var.fruits[count.index]}"
   command="echo ${length(var.fruits)}}"
   }
 }
