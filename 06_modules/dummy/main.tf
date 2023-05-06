f09d4b8813ef4e95ef3206399389bfd26887efef
sameeryalla <sameeryalla@gmail.com>
2023-05-06 15:22:44 +0530

    Terraform module explanation 6

---
+++ 06_modules/dummy/main.tf
@@ -0,0 +1,3 @@
+output "dummy_op"{
+  value = "DevOps"
+}
--- 06_modules/main.tf
+++ 06_modules/main.tf
@@ -1,4 +1,9 @@
 module "sample"{
   source="./module"
+  input=module.dummy.dummy_op
+}
+
+module "dummy"{
+  source="./dummy"
   input="DevOps"
 }
