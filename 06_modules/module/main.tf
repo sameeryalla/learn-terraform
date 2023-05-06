a5ca869015d9b1c3c58852010e7ce1214b673014
sameeryalla <sameeryalla@gmail.com>
2023-05-06 15:02:19 +0530

    Terraform module explanation 3

--- 06_modules/module/main.tf
+++ 06_modules/module/main.tf
@@ -1,24 +1,24 @@
 resource "null_resource" "nothing1"{
-  provisioners "local-exec"{
+  provisioner "local-exec"{
     command="echo Hello from module1"
   }
 
 }
 
 resource "null_resource" "nothing2"{
-  provisioners "local-exec"{
+  provisioner "local-exec"{
     command="echo Hello from module2"
   }
 
 }
 resource "null_resource" "nothing3"{
-  provisioners "local-exec"{
+  provisioner "local-exec"{
     command="echo Hello from module3"
   }
 
 }
 resource "null_resource" "nothing4"{
-  provisioners "local-exec"{
+  provisioner "local-exec"{
     command="echo Hello from module4"
   }
 
